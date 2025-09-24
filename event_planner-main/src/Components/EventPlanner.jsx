59bb581182c541cc4d42773727dedc0b8425c764
Aidriane Carl Esmeña <aidrianecarlesmena@gmail.com>
2025-09-24 10:34:02 +0800

    second commit
    description and events_categories

--- event_planner-main/src/Components/EventPlanner.jsx
+++ event_planner-main/src/Components/EventPlanner.jsx
@@ -7,6 +7,51 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+            <section className='description'>
+                <p>
+                    Plan and organize your events effortlessly with Event Planner. From
+                    birthdaysto corporate meetings, we've got you covered.
+                </p>
+                <button className='get-started-button'>Get Started</button>
+            </section>
+            <section className='events_categories'>
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birtday Parties</li>
+                    <li>Anniversary celebration</li>
+                    <li>Wedding receptions</li>
+                    <li>Baby shower</li>
+                    <li>Graduation parties</li>
+                    <li>Family reunion</li>
+                </ul>
+                <ul>
+                    <h2>Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music festival</li>
+                    <li>Film screening</li>
+                    <li>Comedy shows</li>
+                    <li>art exhibitaion</li>
+                    <li>Cultural events</li>
+                </ul>
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising Events</li>
+                    <li>Charity galas</li>
+                    <li>Volunteer drives</li>
+                    <li>Neiborhood block parties</li>
+                    <li>Community festivals</li>
+                    <li>Cultural celebrations</li>
+                </ul>
+            </section>
+            <section className='features'>
+
+            </section>
+            <section className='testimonials'>
+
+            </section>
+            <section className='contact'>
+
+            </section>
         </div>
     );
 };
